3e9d99aabdbf05d32a85bc204ef0190d029c54f4
davislf2.net@gmail.com <davislf2.net@gmail.com>
2017-10-03 22:50:31 +1100

    add img and name to home screen

--- src/app/pages/HomeScreen/HomeScreen.spec.js
+++ src/app/pages/HomeScreen/HomeScreen.spec.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import { HomeScreenComponent, HomeScreenButtons } from './HomeScreen';
 import userdb from '../../../app/data/userdb.json';
-import Date from '../../../framework/components/Date/Date';
-import Time from '../../../framework/components/Time/Time';
+// import Date from '../../../framework/components/Date/Date';
+// import Time from '../../../framework/components/Time/Time';
 import ButtonAction from '../../../framework/util/ButtonAction';
 
 jest.mock('../../../framework/util/ButtonAction');
@@ -12,7 +12,7 @@ describe('HomeScreenComponent component', () => {
   let homeScreenComponent;
   beforeEach(() => {
     jest.spyOn(ButtonAction, 'goToPage', 'scrollUp', 'scrollDown');
-    homeScreenComponent = shallow(<HomeScreenComponent userdb={ userdb }  />);
+    homeScreenComponent = shallow(<HomeScreenComponent userdb={ userdb } />);
   });
 
   test('it should have a LEFT button config of going to Counter Page with an initial number value of 5', () => {
@@ -38,6 +38,7 @@ describe('HomeScreenComponent component', () => {
     expect(homeScreenComponent).toContainReact(<h2>Davis</h2>);
   });
   test('it should have an image', () => {
-     expect(homeScreenComponent).toContainReact(<img src='../../../app/pages/HomeScreen/profile.png' alt='User Profile' style={ { width: '100px' } } />);
+    expect(homeScreenComponent).toContainReact(
+      <img src='../../../app/pages/HomeScreen/profile.png' alt='User Profile' style={ { width: '100px' } } />);
   });
 });

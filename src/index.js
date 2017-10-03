ae88d81931c4c25cbdc48358585731daa328a18c
davislf2.net@gmail.com <davislf2.net@gmail.com>
2017-10-03 22:56:42 +1100

    add img and name to home screen

--- src/index.js
+++ src/index.js
@@ -7,11 +7,10 @@ import ContactScreen from './app/pages/ContactListScreen/ContactListScreen';
 import NotFoundScreen from './app/pages/NotFoundScreen/NotFoundScreen';
 import JobInfoScreen from './app/pages/JobDetailScreen/JobDetailScreen';
 import contacts from './app/data/contacts.json';
-import jobs from './app/data/jobdb.json';
-import userdb from './app/data/userdb.json';
 import WatchApp from './framework';
 import JobPostScreen from './app/pages/JobPostScreen/JobPostScreen';
 import jobs from './app/data/jobdb.json';
+import userdb from './app/data/userdb.json';
 
 const pages = [
   { path: '/', Component: HomeScreen, props: { userdb } },
